822dad84b2e6db3c3f9adf48cc8e22375b39f514
Isaac Jordan <isaacjordan95@gmail.com>
2020-02-29 13:33:57 +0000

    Add move recommendations based on current board state

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # camel-up-simulation
+
+A small tool to cheat at Camel Up.
--- simulate.py
+++ simulate.py
@@ -15,9 +15,9 @@ CAMEL_ID_TO_COLOUR = dict(reversed(item) for item in CAMEL_COLOUR_TO_ID.items())
 GAMEBOARD_FROM_COLOURS = [
     [],  # 1
     ["yellow", "orange"],  # 2
-    ["blue", "white", "green"],  # 3
+    ["blue", "white"],  # 3
     [],  # 4
-    [],  # 5
+    ["green"],  # 5
     [],  # 6
     [],  # 7
     [],  # 8
@@ -31,7 +31,14 @@ GAMEBOARD_FROM_COLOURS = [
     []  # 16
 ]
 
+# Position: modifier
+# E.g. 4: -1 would mean there is a -1 movement desert tile on slot 4 of the board
+DESERT_TILES = {
+    4: -1
+}
 
+# Convert to array indices instead of gameboard slots
+DESERT_TILES = {tile - 1: modifier for (tile, modifier) in DESERT_TILES.items()}
 
 # Use for simulating a full game
 GAMEBOARD = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
@@ -42,8 +49,7 @@ if GAMEBOARD_FROM_COLOURS:
         for camel_colour in contents:
             GAMEBOARD[slot] += [CAMEL_COLOUR_TO_ID[camel_colour]]
 
-print("Starting gameboard:")
-print(GAMEBOARD)
+print("Starting gameboard:\n", GAMEBOARD_FROM_COLOURS or GAMEBOARD)
 
 
 CAMELS_LEFT_TO_ROLL = set(CAMELS)
@@ -73,6 +79,7 @@ def find_camel(camel_to_find):
 
 
 def simuluate_turn():
+    global WINNER
     camel_to_move, amount_to_move = roll_dice()
 
     # print("Moving camel", camel_to_move, "for", amount_to_move)
@@ -90,15 +97,31 @@ def simuluate_turn():
         camel_stack_new_position = slot_index + amount_to_move
 
         if camel_stack_new_position >= 15:
-            global WINNER
             WINNER = camel_to_move  # Winner of the game
             return
 
-        # Move them onto whatever stack is at the new position
-        GAMEBOARD[camel_stack_new_position] += camel_stack_to_move
-
         # Remove the stack from the position we moved them from
         GAMEBOARD[slot_index] = GAMEBOARD[slot_index][:layer_index]
+
+        if camel_stack_new_position in DESERT_TILES.keys():
+            # print("Desert tile in play")
+            # There is a desert tile in the new location
+            if DESERT_TILES[camel_stack_new_position] > 0:
+
+                camel_stack_new_position += 1
+                if camel_stack_new_position >= 15:
+                    WINNER = camel_to_move  # Winner of the game
+                    return
+
+                # Move them onto whatever stack is at the new position
+                GAMEBOARD[camel_stack_new_position] += camel_stack_to_move
+            else:
+                camel_stack_new_position -= 1
+                # Move them to the bottom of any stack already there
+                GAMEBOARD[camel_stack_new_position] = camel_stack_to_move + GAMEBOARD[camel_stack_new_position]
+        else:
+            # Move them onto whatever stack is at the new position
+            GAMEBOARD[camel_stack_new_position] += camel_stack_to_move
 
     # print(GAMEBOARD)
     return None
@@ -115,20 +138,17 @@ def get_current_positions():
 def simulate_leg():
     while len(CAMELS_LEFT_TO_ROLL) > 0:
         simuluate_turn()
-    # print("End of round state:")
-    # print(GAMEBOARD)
 
 
 def simulate_game():
     while WINNER is None:
         simulate_leg()
         reset_pyramind()
-    print("Final game state:")
-    print(GAMEBOARD)
-    print("WINNER:", WINNER)
+    # print("Final game state:\n", GAMEBOARD)
+    # print("WINNER:", WINNER)
 
 
-def probabilities_for_leg():
+def print_recommendations_for_leg():
     global GAMEBOARD, WINNER, CAMELS_LEFT_TO_ROLL
     starting_state = copy.deepcopy(GAMEBOARD)
     starting_camels_to_roll = copy.deepcopy(CAMELS_LEFT_TO_ROLL)
@@ -136,25 +156,73 @@ def probabilities_for_leg():
     times_first = {CAMEL_ID_TO_COLOUR[camel_id]: 0 for camel_id in CAMELS}
     times_second = {CAMEL_ID_TO_COLOUR[camel_id]: 0 for camel_id in CAMELS}
 
-    n_simulations = 10000
+    n_simulations = 50000
 
     for i in range(n_simulations):
-        # print("GAMEBOARD start", GAMEBOARD)
         simulate_leg()
         positions = get_current_positions()
-        # print(positions)
 
         times_first[CAMEL_ID_TO_COLOUR[positions[0]]] += 1
         times_second[CAMEL_ID_TO_COLOUR[positions[1]]] += 1
 
         GAMEBOARD = copy.deepcopy(starting_state)
         CAMELS_LEFT_TO_ROLL = copy.deepcopy(starting_camels_to_roll)
+        WINNER = None
+        # print("Reset board back to:\n", GAMEBOARD)
 
-    for camel in sorted(times_first, key=times_first.get, reverse=True):
-        print("Camel", camel, "wins", round(times_first[camel] / n_simulations * 100.0), "%")
+    print("\nRecommendations for short-term bets:")
+    for gold_value_of_bet in [5, 3, 2]:
+        expected_values = {camel: gold_value_of_bet * (times_first[camel] / n_simulations) for camel in times_first.keys()}
 
-    for camel in sorted(times_second, key=times_second.get, reverse=True):
-        print("Camel", camel, "comes second", round(times_second[camel] / n_simulations * 100.0), "%")
+        # Add on value of it coming second (reward: 1)
+        expected_values = {camel: expected_values[camel] + (times_second[camel] / n_simulations) for camel in times_second.keys()}
+
+        for camel in sorted(expected_values, key=expected_values.get, reverse=True):
+            if expected_values[camel] > 1:
+                print("Camel", camel, "expected value at bet", gold_value_of_bet, " is ", round(expected_values[camel], 1))
+    print("If none of these are available, do a long term bet, place a road block, or roll a dice")
 
 
-probabilities_for_leg()
+def print_recommendations_for_game():
+    global GAMEBOARD, WINNER, CAMELS_LEFT_TO_ROLL
+    starting_state = copy.deepcopy(GAMEBOARD)
+    starting_camels_to_roll = copy.deepcopy(CAMELS_LEFT_TO_ROLL)
+
+    times_first = {CAMEL_ID_TO_COLOUR[camel_id]: 0 for camel_id in CAMELS}
+    times_last = {CAMEL_ID_TO_COLOUR[camel_id]: 0 for camel_id in CAMELS}
+
+    n_simulations = 50000
+
+    for i in range(n_simulations):
+        simulate_game()
+        positions = get_current_positions()
+
+        times_first[CAMEL_ID_TO_COLOUR[WINNER]] += 1
+        times_last[CAMEL_ID_TO_COLOUR[positions[-1]]] += 1
+
+        GAMEBOARD = copy.deepcopy(starting_state)
+        CAMELS_LEFT_TO_ROLL = copy.deepcopy(starting_camels_to_roll)
+        WINNER = None
+
+    print("\nRecommendations for long-term bets:")
+    expected_values_for_first = {camel: 8 * (times_first[camel] / n_simulations) for camel in
+                       times_first.keys()}
+
+    # Add on value of it coming second (reward: 1)
+    expected_values_for_last = {camel: 8 * (times_last[camel] / n_simulations) for camel in
+                       times_last.keys()}
+
+    for camel in sorted(expected_values_for_first, key=expected_values_for_first.get, reverse=True):
+        if expected_values_for_first[camel] > 1:
+            print("Camel", camel, "expected value to win is ", round(expected_values_for_first[camel], 1))
+
+    print("")
+    for camel in sorted(expected_values_for_last, key=expected_values_for_last.get, reverse=True):
+        if expected_values_for_last[camel] > 1:
+            print("Camel", camel, "expected value to come last is ", round(expected_values_for_last[camel], 1))
+
+    print("If none of these are available, place a road block, or roll a dice")
+
+
+print_recommendations_for_leg()
+print_recommendations_for_game()
