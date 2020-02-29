6765d424b944eea5268576afd5b9370e6f213848
Isaac Jordan <isaacjordan95@gmail.com>
2020-02-29 12:12:23 +0000

    Add ability to simulate the results of a leg

--- simulate.py
+++ simulate.py
@@ -1,15 +1,21 @@
 from random import randint, choice
+import copy
 
-camels = [1, 2, 3, 4, 5]
+CAMELS = [1, 2, 3, 4, 5]
+CAMEL_COLOUR_TO_ID = {
+    "orange": 1,
+    "green": 2,
+    "blue": 3,
+    "yellow": 4,
+    "white": 5
+}
+CAMEL_ID_TO_COLOUR = dict(reversed(item) for item in CAMEL_COLOUR_TO_ID.items())
 
-# Use for simulating a full game
-# GAMEBOARD = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
-
-# Use for simulating from partial state
-GAMEBOARD = [
+# Camel on right-hand-side is on top
+GAMEBOARD_FROM_COLOURS = [
     [],  # 1
-    [4, 1],  # 2
-    [3, 5, 2],  # 3
+    ["yellow", "orange"],  # 2
+    ["blue", "white", "green"],  # 3
     [],  # 4
     [],  # 5
     [],  # 6
@@ -25,17 +31,28 @@ GAMEBOARD = [
     []  # 16
 ]
 
+
+
+# Use for simulating a full game
+GAMEBOARD = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+
+# Populate from human-friendly names
+if GAMEBOARD_FROM_COLOURS:
+    for slot, contents in enumerate(GAMEBOARD_FROM_COLOURS):
+        for camel_colour in contents:
+            GAMEBOARD[slot] += [CAMEL_COLOUR_TO_ID[camel_colour]]
+
 print("Starting gameboard:")
 print(GAMEBOARD)
 
 
-CAMELS_LEFT_TO_ROLL = set()
+CAMELS_LEFT_TO_ROLL = set(CAMELS)
 WINNER = None
 
 
 def reset_pyramind():
     global CAMELS_LEFT_TO_ROLL
-    CAMELS_LEFT_TO_ROLL = set(camels)
+    CAMELS_LEFT_TO_ROLL = set(CAMELS)
 
 
 def roll_dice():
@@ -58,7 +75,7 @@ def find_camel(camel_to_find):
 def simuluate_turn():
     camel_to_move, amount_to_move = roll_dice()
 
-    print("Moving camel", camel_to_move, "for", amount_to_move)
+    # print("Moving camel", camel_to_move, "for", amount_to_move)
 
     # Slot = GAMEBOARD position, layer = position in stack of camels
     slot_index, layer_index = find_camel(camel_to_move)
@@ -83,24 +100,61 @@ def simuluate_turn():
         # Remove the stack from the position we moved them from
         GAMEBOARD[slot_index] = GAMEBOARD[slot_index][:layer_index]
 
-    print(GAMEBOARD)
+    # print(GAMEBOARD)
     return None
 
 
+def get_current_positions():
+    order = []
+    for slot in reversed(GAMEBOARD):
+        for camel in reversed(slot):
+            order += [camel]
+    return order
+
+
 def simulate_leg():
-    reset_pyramind()
     while len(CAMELS_LEFT_TO_ROLL) > 0:
         simuluate_turn()
-    print("End of round state:")
-    print(GAMEBOARD)
+    # print("End of round state:")
+    # print(GAMEBOARD)
 
 
 def simulate_game():
     while WINNER is None:
         simulate_leg()
+        reset_pyramind()
     print("Final game state:")
     print(GAMEBOARD)
     print("WINNER:", WINNER)
 
 
-simulate_game()
+def probabilities_for_leg():
+    global GAMEBOARD, WINNER, CAMELS_LEFT_TO_ROLL
+    starting_state = copy.deepcopy(GAMEBOARD)
+    starting_camels_to_roll = copy.deepcopy(CAMELS_LEFT_TO_ROLL)
+
+    times_first = {CAMEL_ID_TO_COLOUR[camel_id]: 0 for camel_id in CAMELS}
+    times_second = {CAMEL_ID_TO_COLOUR[camel_id]: 0 for camel_id in CAMELS}
+
+    n_simulations = 10000
+
+    for i in range(n_simulations):
+        # print("GAMEBOARD start", GAMEBOARD)
+        simulate_leg()
+        positions = get_current_positions()
+        # print(positions)
+
+        times_first[CAMEL_ID_TO_COLOUR[positions[0]]] += 1
+        times_second[CAMEL_ID_TO_COLOUR[positions[1]]] += 1
+
+        GAMEBOARD = copy.deepcopy(starting_state)
+        CAMELS_LEFT_TO_ROLL = copy.deepcopy(starting_camels_to_roll)
+
+    for camel in sorted(times_first, key=times_first.get, reverse=True):
+        print("Camel", camel, "wins", round(times_first[camel] / n_simulations * 100.0), "%")
+
+    for camel in sorted(times_second, key=times_second.get, reverse=True):
+        print("Camel", camel, "comes second", round(times_second[camel] / n_simulations * 100.0), "%")
+
+
+probabilities_for_leg()
